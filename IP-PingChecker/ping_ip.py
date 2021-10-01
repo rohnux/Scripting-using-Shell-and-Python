2165df45fe73af2b4464867b135aa7951a07f624
Rohan Gupta <rohangupta.mnnit@gmail.com>
2021-10-01 20:51:02 +0530

    Created ping.py to check ip connectivity

---
+++ IP-PingChecker/ping_ip.py
@@ -0,0 +1,29 @@
+import subprocess
+
+
+def pingcheck(servers): 
+      print(servers)
+      cmd = 'ping'
+      # send one packet of data to the host 
+      # this is specified by '-c 1' in the argument list 
+      outputlist = []
+      # Iterate over all the servers in the list and ping each server
+      for server in servers:
+          temp = subprocess.Popen([cmd, '-c 1', server], stdout = subprocess.PIPE) 
+          # get the output as a string
+          output = str(temp.communicate()) 
+      # store the output in the list
+          outputlist.append(output)
+      return outputlist
+
+
+servers=list(open("test.txt"))
+#print(servers)
+outputlist=[]
+
+for i in range(len(servers)):
+      servers[i] = servers[i].strip('\n')
+outputlist=pingcheck(servers)
+print(outputlist)
+
+
